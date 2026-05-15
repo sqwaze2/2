81970d3df7de5c112363f49109ef4e609fa984ad
sqwaze2 <svetlanazulkarnaeva19@gmail.com>
2026-05-16 02:24:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ async def build_message():
 
     
     lines = []
-    lines.append("** OUR GAMES **")
+    lines.append("## ** OUR GAMES **")
 
     for uid, (name, status, players, link) in combined:
         status_text = "Active" if status else "Down"
@@ -101,7 +101,7 @@ async def build_message():
     group_name, member_count, is_locked = group_result
     if not is_locked:
         group_link = f"https://www.roblox.com/groups/{GROUP_ID}"
-        lines.append("\n** OUR GROUP **")
+        lines.append("\n ## ** OUR GROUP **")
         lines.append(
             f"**{group_name}**\n"
             f"> * Members: {member_count:,} 👥\n"
